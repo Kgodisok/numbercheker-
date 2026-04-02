383437644dd68bd7512d32c9d5a2b7ed1f0f4b99
Kgodisok <kgodisomatsepe474@gmail.com>
2026-04-02 14:50:14 +0200

    Fix variable name for user input in even or odd check

--- evenorodd.js
+++ evenorodd.js
@@ -3,8 +3,8 @@ const prompt = require("prompt-sync")();
 
 const ask = () =>{
     while (true){
-        var question = prompt("Enter a number here: ")
-        if (isNaN(question)){
+        var number = prompt("Enter a number here: ")
+        if (isNaN(number)){
             console.log("Please enter a valid number.")
         }else if(question%2 === 0){
             console.log("Even Number.")
